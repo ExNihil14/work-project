c7b9b097b520de8701b597c27b07891f9340f565
ExNihil14 <ExNihil88@gmail.com>
2016-12-04 00:56:30 +0300

    Init

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,3 +1,23 @@
 console.log('\'Allo \'Allo!');
 
-$('.Mycarusel').slick();
+$(document).ready(function(){
+
+ 	$('.Mycarusel').slick({
+
+    	prevArrow:"<img class='a-left control-c prev slick-prev' src='../images/back.png'>",
+    	nextArrow:"<img class='a-right control-c next slick-next' src='../images/right.png'>"
+
+	});
+
+	$('.Mycarusel2').slick({
+
+		prevArrow:"<img class='a-left control-c prev slick-prev' src='../images/prev.png'>",
+	    nextArrow:"<img class='a-right control-c next slick-next' src='../images/next.png'>",
+
+	    slidesToShow: 4,
+		slidesToScroll: 1
+
+	});
+
+});
+
