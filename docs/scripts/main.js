7eb8fdb5022e44c4b5586414d54f4bf9fdac80f0
ExNihil14 <ExNihil88@gmail.com>
2016-12-04 22:11:38 +0300

    Init

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -19,5 +19,12 @@ $(document).ready(function(){
 
 	});
 
+	$('.Mycarusel3').slick({
+
+		prevArrow:"<img class='a-left control-c prev slick-prev' src='../images/prev.png'>",
+	    nextArrow:"<img class='a-right control-c next slick-next' src='../images/next.png'>"
+	    
+	});
+
 });
 
