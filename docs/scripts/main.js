f175025f238a04ca7e68f75ef65601479a6c474d
ExNihil14 <ExNihil88@gmail.com>
2016-12-06 11:40:34 +0300

    Init

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,30 +1,31 @@
+'use strict';
+
 console.log('\'Allo \'Allo!');
 
-$(document).ready(function(){
+$(document).ready(function () {
 
- 	$('.Mycarusel').slick({
+	$('.Mycarusel').slick({
 
-    	prevArrow:"<img class='a-left control-c prev slick-prev' src='images/back.png'>",
-    	nextArrow:"<img class='a-right control-c next slick-next' src='images/right.png'>"
+		prevArrow: "<img class='a-left control-c prev slick-prev' src='images/back.png'>",
+		nextArrow: "<img class='a-right control-c next slick-next' src='images/right.png'>"
 
 	});
 
 	$('.Mycarusel2').slick({
 
-		prevArrow:"<img class='a-left control-c prev slick-prev' src='images/prev.png'>",
-	    nextArrow:"<img class='a-right control-c next slick-next' src='images/next.png'>",
+		prevArrow: "<img class='a-left control-c prev slick-prev' src='images/prev.png'>",
+		nextArrow: "<img class='a-right control-c next slick-next' src='images/next.png'>",
 
-	    slidesToShow: 4,
+		slidesToShow: 4,
 		slidesToScroll: 1
 
 	});
 
 	$('.Mycarusel3').slick({
 
-		prevArrow:"<img class='a-left control-c prev slick-prev' src='images/prev.png'>",
-	    nextArrow:"<img class='a-right control-c next slick-next' src='images/next.png'>"
-	    
+		prevArrow: "<img class='a-left control-c prev slick-prev' src='images/prev.png'>",
+		nextArrow: "<img class='a-right control-c next slick-next' src='images/next.png'>"
+
 	});
-
 });
-
+//# sourceMappingURL=main.js.map
